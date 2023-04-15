343df72d1d41a56b532b18dcfd254a6e4313561e
NaZe0320 <rwt1226@naver.com>
2023-04-16 03:03:50 +0900

    [feat] ContextExtension.kt 추가

---
+++ app/src/main/java/com/naze/objectoneshot_ver2/util/ContextExtension.kt
@@ -0,0 +1,8 @@
+package com.naze.objectoneshot_ver2.util
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
