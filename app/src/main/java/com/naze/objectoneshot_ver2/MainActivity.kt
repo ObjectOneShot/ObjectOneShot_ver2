8be281c8b1bc560947b38fd40477aff69b0ad0d0
NaZe0320 <rwt1226@naver.com>
2023-04-16 01:35:50 +0900

    [init]

---
+++ app/src/main/java/com/naze/objectoneshot_ver2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.naze.objectoneshot_ver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
