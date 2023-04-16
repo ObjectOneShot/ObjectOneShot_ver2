11251c983d0f9270dc17910baa805050339ff4ed
NaZe0320 <rwt1226@naver.com>
2023-04-17 02:17:13 +0900

    [feat] main backpress 구현

--- app/src/main/java/com/naze/objectoneshot_ver2/presentation/MainActivity.kt
+++ app/src/main/java/com/naze/objectoneshot_ver2/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.naze.objectoneshot_ver2.presentation
 import android.content.Context
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -11,11 +12,13 @@ import com.naze.objectoneshot_ver2.R
 import com.naze.objectoneshot_ver2.databinding.ActivityMainBinding
 import com.naze.objectoneshot_ver2.presentation.objective.ObjectiveListFragment
 import com.naze.objectoneshot_ver2.util.BindingActivity
+import com.naze.objectoneshot_ver2.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var imm: InputMethodManager
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_ObjectOneShot_ver2)
@@ -32,8 +35,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-
-
         if (event.action == MotionEvent.ACTION_DOWN) {
             val v = currentFocus
             if (v is EditText) {
@@ -46,5 +47,28 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             }
         }
         return super.onTouchEvent(event)
+    }
+
+    override fun onBackPressed() {
+        for (fragment: Fragment in supportFragmentManager.fragments) {
+            when (fragment.tag) {
+                "ObjectiveList" -> {
+                    val currentTime = System.currentTimeMillis()
+                    if (currentTime - backPressedTime < 2000) {
+                        super.onBackPressed()
+                    } else {
+                        backPressedTime = currentTime
+                        this.showToast("한 번 더 누르면 종료됩니다.")
+                    }
+                }
+                "KeyResultModify" -> {
+                    Log.d("TEST_MAIN","onBackPressed $fragment")
+                    super.onBackPressed()
+                }
+                else -> {
+                    supportFragmentManager.popBackStackImmediate()
+                }
+            }
+        }
     }
 }
