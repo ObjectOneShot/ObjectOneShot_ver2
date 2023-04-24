d64b6a9cf334c4a05a9c08bfdaad233fa1888727
NaZe0320 <rwt1226@naver.com>
2023-04-24 22:10:16 +0900

    [feat] ObjectiveAchieveListFragment viewmodel 교체

--- app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/objective/ObjectiveAchieveFragment.kt
+++ app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/objective/ObjectiveAchieveFragment.kt
@@ -7,12 +7,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.objectiveoneshot.objectiveoneshot.R
 import com.objectiveoneshot.objectiveoneshot.databinding.FragmentObjectiveAchieveListBinding
-import com.objectiveoneshot.objectiveoneshot.domain.viewmodel.ObjectiveViewModel
+import com.objectiveoneshot.objectiveoneshot.domain.viewmodel.AppViewModel
 import com.objectiveoneshot.objectiveoneshot.presentation.tips.TipsFragment
 import com.objectiveoneshot.objectiveoneshot.util.BindingFragment
 
 class ObjectiveAchieveFragment: BindingFragment<FragmentObjectiveAchieveListBinding>(R.layout.fragment_objective_achieve_list) {
-    private val objectiveViewModel : ObjectiveViewModel by activityViewModels()
+    private val viewModel : AppViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,21 +30,21 @@ class ObjectiveAchieveFragment: BindingFragment<FragmentObjectiveAchieveListBind
     @SuppressLint("ClickableViewAccessibility")
     private fun setRecyclerView() {
         val adapterObjective = ObjectiveAchieveListAdapter(ObjectiveAchieveListAdapter.ItemClickListener {
-             objectiveViewModel.getObjectiveData(it)
+            viewModel.getObjectiveData(it)
             parentFragmentManager.beginTransaction().apply {
                 replace(R.id.fl_main, ObjectiveAchieveViewFragment(), "ObjectiveAchieveView")
                 addToBackStack(null)
                 commit()
             }
-        },objectiveViewModel)
+        },viewModel)
 
         binding.rvObjective.apply {
             adapter = adapterObjective
             layoutManager = LinearLayoutManager(requireContext())
         }
-        objectiveViewModel.getObjectiveAchieveList()
+        viewModel.getObjectiveAchieveList()
 
-        objectiveViewModel.objectiveListWithKeyResults.observe(viewLifecycleOwner) {
+        viewModel.objectiveListWithKeyResults.observe(viewLifecycleOwner) {
             adapterObjective.submitList(it)
         }
     }
