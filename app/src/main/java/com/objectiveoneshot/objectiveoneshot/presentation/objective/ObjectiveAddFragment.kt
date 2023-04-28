daa93bf82bcc1f5ced0dc128db8d3a1faf8168ac
NaZe0320 <rwt1226@naver.com>
2023-04-28 11:11:17 +0900

    [feat] keyResult 저장 기능 삭제 후 자동 변경 감지

--- app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/objective/ObjectiveAddFragment.kt
+++ app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/objective/ObjectiveAddFragment.kt
@@ -7,21 +7,17 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.objectiveoneshot.objectiveoneshot.R
 import com.objectiveoneshot.objectiveoneshot.databinding.FragmentObjectiveAddBinding
 import com.objectiveoneshot.objectiveoneshot.domain.type.KeyResultState
 import com.objectiveoneshot.objectiveoneshot.domain.viewmodel.AppViewModel
 import com.objectiveoneshot.objectiveoneshot.presentation.keyresult.KeyResultListFragment
-import com.objectiveoneshot.objectiveoneshot.presentation.task.TaskAddAdapter
 import com.objectiveoneshot.objectiveoneshot.presentation.tips.TipsFragment
 import com.objectiveoneshot.objectiveoneshot.util.BindingFragment
-import com.objectiveoneshot.objectiveoneshot.util.showKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -56,7 +52,7 @@ class ObjectiveAddFragment: BindingFragment<FragmentObjectiveAddBinding>(R.layou
         })
 
         binding.btnAddObjective.setOnClickListener {
-            if (binding.keyAddItem.layoutKeyAdd.visibility == View.GONE) {
+            if (true) {
                 viewModel.insertObjectiveData()
                 parentFragmentManager.popBackStackImmediate()
             } else {
@@ -106,68 +102,11 @@ class ObjectiveAddFragment: BindingFragment<FragmentObjectiveAddBinding>(R.layou
     }
 
     /**  Add Key Result   */
-    private lateinit var adapterTask: TaskAddAdapter
     private fun setAddKeyResult () {
-        var id = ""
         binding.btnAddKeyResult.setOnClickListener { //Add btn 을 눌렀을 때
-            if (binding.keyAddItem.layoutKeyAdd.visibility == View.VISIBLE) { //추가 상태일 때
-                if (requireView().hasFocus()) {
-                    requireView().clearFocus()
-                }
-                if (binding.keyAddItem.etKeyName.text.toString().isNotEmpty()) { //KeyResult 명이 비어있지 않으면
-                    if (adapterTask.currentList[0].content.isNotEmpty() || adapterTask.currentList.size > 1) {
-                        //viewModel.addKeyResult() //데이터 입력
-                        setVisibleKeyResult(false)
-                        binding.keyAddItem.etKeyName.setHintTextColor(Color.parseColor("#FF808080"))
-                    } else {
-                        val dialog: Dialog = Dialog(requireContext())
-                        dialog.setContentView(R.layout.dialog_task_alert)
-                        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                        dialog.show()
-                    }
-                } else {
-                    binding.keyAddItem.etKeyName.setHintTextColor(Color.parseColor("#80FF0000"))
-                }
-
-            } else if (binding.keyAddItem.layoutKeyAdd.visibility == View.GONE) {
-                id = viewModel.addKeyResult() //신규 데이터 생성
-
-                setVisibleKeyResult(true)
-                binding.keyAddItem.etKeyName.requestFocus()
-                requireContext().showKeyboard(binding.keyAddItem.etKeyName,true)
-                adapterTask = TaskAddAdapter(id, viewModel)
-
-                binding.keyAddItem.rvTaskList.apply {
-                    adapter = adapterTask
-                    layoutManager = LinearLayoutManager(context)
-                    try { adapterTask.submitList(viewModel.keyResultWithTasks.value?.first { (id) == it.keyResult.id }?.tasks)
-                    } catch (e: NoSuchElementException){ e.printStackTrace() }
-                }
-            }
-        }
-        binding.keyAddItem.btnDeleteKey.setOnClickListener {
-            setVisibleKeyResult(false)
-            //저장된 테스크 데이터 삭제
-            viewModel.deleteKeyResult(id)
-        }
-        binding.keyAddItem.etKeyName.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                binding.keyAddItem.rvTaskList.requestFocus()
-                return@setOnEditorActionListener true
-            } else {
-                false
-            }
+            viewModel.addKeyResult()
         }
 
-    }
-    private fun setVisibleKeyResult(isAdd: Boolean) { //추가인지 저장인지
-        if (isAdd) {
-            binding.keyAddItem.layoutKeyAdd.visibility = View.VISIBLE
-            binding.btnAddKeyResult.setImageResource(R.drawable.ic_text_keyresult_save)
-        } else {
-            binding.keyAddItem.layoutKeyAdd.visibility = View.GONE
-            binding.btnAddKeyResult.setImageResource(R.drawable.ic_text_key_result_add)
-        }
     }
 
     private fun keyResultStateFragmentSetting() {
