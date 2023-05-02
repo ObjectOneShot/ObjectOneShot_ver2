58a650f07d22a68e93a802b2c5284d84d9adf86f
NaZe0320 <rwt1226@naver.com>
2023-05-03 01:11:40 +0900

    [feat] GA 추가

--- app/src/main/java/com/objectiveoneshot/objectiveoneshot/util/BindingFragment.kt
+++ app/src/main/java/com/objectiveoneshot/objectiveoneshot/util/BindingFragment.kt
@@ -1,6 +1,7 @@
 package com.objectiveoneshot.objectiveoneshot.util
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +9,41 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
 abstract class BindingFragment<B: ViewDataBinding>(@LayoutRes private val layoutRes: Int): Fragment() {
     private var _binding: B? = null
     val binding get() = requireNotNull(_binding!!) { "${this::class.java.simpleName}에서 에러가 발생했습니다."}
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+
+        firebaseAnalytics = Firebase.analytics
+        recordScreenView()
         return binding.root
+    }
+
+    private fun analyticSelect(id: String, name: String, contentType: String) {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_ID, id)
+            param(FirebaseAnalytics.Param.ITEM_NAME,name)
+            param(FirebaseAnalytics.Param.CONTENT_TYPE,contentType)
+        }
+    }
+
+    private fun recordScreenView() {
+        val screenName = this::class.java.simpleName
+        Log.d("TEST_analytics","TEST $screenName")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+        }
     }
 }
