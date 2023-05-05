569bf747166d1f8f0904c11787f66fcccbb43006
NaZe0320 <rwt1226@naver.com>
2023-05-05 12:24:07 +0900

    [feat] unedit 뷰 수정

--- app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/keyresult/KeyResultUnEditAdapter.kt
+++ app/src/main/java/com/objectiveoneshot/objectiveoneshot/presentation/keyresult/KeyResultUnEditAdapter.kt
@@ -1,16 +1,17 @@
 package com.objectiveoneshot.objectiveoneshot.presentation.keyresult
 
-import android.util.Log
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.objectiveoneshot.objectiveoneshot.data.local.model.KeyResultWithTasks
+import com.objectiveoneshot.objectiveoneshot.databinding.ItemKeyResultExpandUneditBinding
 import com.objectiveoneshot.objectiveoneshot.databinding.ItemKeyResultUneditBinding
 import com.objectiveoneshot.objectiveoneshot.domain.viewmodel.AppViewModel
-import com.objectiveoneshot.objectiveoneshot.presentation.task.TaskListUnEditAdapter
+import com.objectiveoneshot.objectiveoneshot.presentation.task.TaskListAdapter
 import com.objectiveoneshot.objectiveoneshot.util.ItemDiffCallback
 
 
@@ -22,45 +23,92 @@ class KeyResultUnEditAdapter(
         onItemsTheSame = {old, new -> old.keyResult.id == new.keyResult.id}
     )
 ) {
-
+    companion object {
+        const val TYPE_EXPAND = 0
+        const val TYPE_NORMAL = 1
+    }
     inner class KeyViewHolder(
         private val binding: ItemKeyResultUneditBinding
     ): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("ClickableViewAccessibility")
         fun bind(keyResult: KeyResultWithTasks) {
             binding.keyResult = keyResult
             binding.executePendingBindings()
-            Log.d("TEST_KeyResultAdapter","KeyResult Id : ${keyResult.keyResult.id}")
-            val taskListAdapter = TaskListUnEditAdapter(keyResult.keyResult.id)
+
+            binding.btnExpand.setOnClickListener {
+                getItem(adapterPosition).keyResult.isExpand = true
+                notifyItemChanged(adapterPosition)
+            }
+            binding.etKeyName.setOnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    binding.etKeyName.clearFocus()
+                    return@setOnEditorActionListener true
+                }
+                false
+            }
+        }
+    }
+
+    inner class KeyViewExpandHolder(
+        private val binding: ItemKeyResultExpandUneditBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("ClickableViewAccessibility")
+        fun bind(keyResult: KeyResultWithTasks) {
+            binding.keyResult = keyResult
+            binding.executePendingBindings()
+            val taskListAdapter = TaskListAdapter(keyResult.keyResult.id, viewModel)
 
             binding.rvTaskList.apply {
                 adapter = taskListAdapter
                 layoutManager = LinearLayoutManager(context)
-                taskListAdapter.submitList(viewModel.keyResultWithTasks.value?.first { (keyResult.keyResult.id) == it.keyResult.id }?.tasks)
-                //objectiveViewModel 에서 해당 keyResult에 해당하는 TaskList 가져오기
+
+                try {
+                    taskListAdapter.submitList(viewModel.keyResultWithTasks.value?.first { (keyResult.keyResult.id) == it.keyResult.id }?.tasks)
+                } catch (e: NoSuchElementException){
+                    e.printStackTrace()
+                }
             }
             binding.btnExpand.setOnClickListener {
-                if (binding.rvTaskList.visibility == View.GONE) { //안 보일 때 보이게 하기
-                    binding.rvTaskList.visibility = View.VISIBLE
-                    binding.btnExpand.rotation = 180f
-                } else {
-                    binding.rvTaskList.visibility = View.GONE
-                    binding.btnExpand.rotation = 360f
+                getItem(adapterPosition).keyResult.isExpand = false
+                notifyItemChanged(adapterPosition)
+            }
+            binding.etKeyName.setOnEditorActionListener { v, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    binding.etKeyName.clearFocus()
+                    return@setOnEditorActionListener true
                 }
+                false
             }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        return KeyViewHolder(ItemKeyResultUneditBinding.inflate(inflater, parent, false))
+        return when(viewType) {
+            TYPE_NORMAL -> {
+                val inflater = LayoutInflater.from(parent.context)
+                KeyViewHolder(ItemKeyResultUneditBinding.inflate(inflater, parent, false))
+            }
+            TYPE_EXPAND -> {
+                val inflater = LayoutInflater.from(parent.context)
+                KeyViewExpandHolder(ItemKeyResultExpandUneditBinding.inflate(inflater, parent, false))
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val keyResult = getItem(position)
-        when (holder) {
-            is KeyViewHolder -> {
-                holder.bind(keyResult)
+        when (holder.itemViewType) {
+            TYPE_NORMAL -> {
+                (holder as KeyViewHolder).bind(keyResult)
+            }
+            TYPE_EXPAND -> {
+                (holder as KeyViewExpandHolder).bind(keyResult)
             }
         }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (getItem(position).keyResult.isExpand) TYPE_EXPAND else TYPE_NORMAL
     }
 }
